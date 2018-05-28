47aae8b9f0254781feffc143835278c7686bd0f4
Kenseoshow <parisbqd@gmail.com>
2018-05-28 11:19:20 +0200

    version 28/05/2018

--- application/views/Visiteur/Inscription.php
+++ application/views/Visiteur/Inscription.php
@@ -4,25 +4,25 @@
 echo form_open('Visiteur/Inscription');
 
 echo form_label("Nom : ", 'lbltNom');
-echo form_input('Nom','',array('pattern' =>'^[a-zA-Z][a-zA-Z0-9]*','required'=>'required')).'<BR>';
+echo form_input('Nom','',array('pattern' =>'^[a-zA-Z]{3,8}$','required'=>'required')).'<BR>';
 
 echo form_label("Prenom : ", 'lbltPrenom');
-echo form_input('Prenom','',array('pattern' =>'^[a-zA-Z][a-zA-Z0-9]*','required'=>'required')).'<BR>';
+echo form_input('Prenom','',array('pattern' =>'^[a-zA-Z]{3,8}$','required'=>'required')).'<BR>';
 
 echo form_label("Adresse : ", 'lbltAdresse');
-echo form_input('Adresse','',array('pattern' =>'^[a-zA-Z][a-zA-Z0-9]*','required'=>'required')).'<BR>';
+echo form_input('Adresse','',array('required'=>'required')).'<BR>';
 
 echo form_label("Ville : ", 'lbltVille');
-echo form_input('Ville','',array('pattern' =>'^[a-zA-Z][a-zA-Z0-9]*','required'=>'required')).'<BR>';
+echo form_input('Ville','',array('required'=>'required')).'<BR>';
 
 echo form_label("Code Postal : ", 'lbltCP');
-echo form_input('CodePostal','',array('pattern' =>'^[Z0-9]*','required'=>'required')).'<BR>';
+echo form_input('CodePostal','',array('pattern' =>'^[0-9]{5}$','required'=>'required')).'<BR>';
 
 echo form_label("Email : ", 'lbltEmail');
-echo form_input('Email','',array('pattern' =>'^[a-zA-Z][a-zA-Z0-9]*','required'=>'required')).'<BR>';
+echo form_input('Email','',array('pattern' =>'^[a-zA-Z0-9\-_]+[a-zA-Z0-9\.\-_]*@[a-zA-Z0-9\-_]+\.[a-zA-Z\.\-_]{1,}[a-zA-Z\-_]+','required'=>'required')).'<BR>';
   
 echo form_label("Mot de passe : ", 'lbltMDP');
-echo form_password('MotDePasse','',array('pattern' =>'^[a-zA-Z][a-zA-Z0-9]*','required'=>'required')).'<BR>';
+echo form_password('MotDePasse','',array('required'=>'required')).'<BR>';
 
 echo form_submit('boutonInscription', 'Inscription').'<BR>';
 echo form_close();
--- application/views/templates/Entete.php
+++ application/views/templates/Entete.php
@@ -1,34 +1,50 @@
 <html>
 <head>
 <title>TEST</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
-<a href="<?php echo site_url('visiteur/Home') ?>">Page d'acceuil</a>
-<a href="<?php echo site_url('visiteur/listerLesArticles') ?>">Lister tous les Articles</a>&nbsp;&nbsp;
-<?php if (!is_null($this->session->identifiant)): ?>
-<?php echo 'Utilisateur connecté : <B>'.$this->session->identifiant.'</B>&nbsp;&nbsp;';?>
-<a href="<?php echo site_url('visiteur/seDeconnecter') ?>">Se déconnecter</a>&nbsp;&nbsp;
-
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="<?php echo site_url('visiteur/Home') ?>">Manga</a>&nbsp;&nbsp;
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="<?php echo site_url('visiteur/Home') ?>">Page d'acceuil</a>&nbsp;&nbsp;</li>
+      <li><a href="<?php echo site_url('visiteur/listerLesArticles') ?>">Lister tous les Articles</a>&nbsp;&nbsp;</li>
+      <ul class="nav navbar-nav navbar-right">
+      <?php if (!is_null($this->session->identifiant)): ?>
+      <div class="container">
+      <li><p class="text-success"><?php echo 'Utilisateur connecté : <B>'.$this->session->identifiant.'</B>&nbsp;&nbsp;';?></li>
+      </div>
+      
+      <li><a href="<?php echo site_url('visiteur/seDeconnecter') ?>">Se déconnecter</a>&nbsp;&nbsp;</li>
+      </ul>
       <?php if ($this->session->statut=="user") : ?>
       <a href="<?php echo site_url('Visiteur/ModificationUnCompte')?>">Modifier Compte</a>&nbsp;&nbsp;
       <?php endif; ?>
-              <?php if ($this->session->statut=="admin") : ?>
-              <a href="<?php echo site_url('administrateur/ajouterUnProduit') ?>">Ajouter un article</a>&nbsp;&nbsp;
-              <a href="<?php echo site_url('Administrateur/ajouterUneMarque')?>">Ajouter une Marque</a>&nbsp;&nbsp;
-              <a href="<?php echo site_url('Administrateur/ajouterUneCategorie')?>">Ajouter une Catégorie</a>&nbsp;&nbsp;
-              <a href="<?php echo site_url('Administrateur/SupprimerUnProduit')?>">Supprimer Un Produit</a>&nbsp;&nbsp;
-              <a href="<?php echo site_url('Administrateur/ModificationUnProduit')?>">Modifier Un Produit</a>&nbsp;&nbsp;
-              <a href="<?php echo site_url('Visiteur/ModificationUnCompte')?>">Modifier Compte</a>&nbsp;&nbsp;
-              <?php endif; ?>
+      <?php if ($this->session->statut=="admin") : ?>
+      <li><a href="<?php echo site_url('administrateur/ajouterUnProduit') ?>">Ajouter un article</a>&nbsp;&nbsp;</li>
+      <li><a href="<?php echo site_url('Administrateur/ajouterUneMarque')?>">Ajouter une Marque</a>&nbsp;&nbsp;</li>
+      <li><a href="<?php echo site_url('Administrateur/ajouterUneCategorie')?>">Ajouter une Catégorie</a>&nbsp;&nbsp;</li>
+      <li><a href="<?php echo site_url('Administrateur/SupprimerUnProduit')?>">Supprimer Un Produit</a>&nbsp;&nbsp;</li>
+      <li><a href="<?php echo site_url('Administrateur/ModificationUnProduit')?>">Modifier Un Produit</a>&nbsp;&nbsp;</li>
+      <li><a href="<?php echo site_url('Visiteur/ModificationUnCompte')?>">Modifier Compte</a>&nbsp;&nbsp;</li>
+      <?php endif; ?>
+    </ul>
+  </div>      
 <?php else : ?>
-<a href="<?php echo site_url('visiteur/seConnecter') ?>">Se Connecter</a>&nbsp;&nbsp;
-<a href="<?php echo site_url('visiteur/Inscription') ?>">Inscription</a>&nbsp;&nbsp;
+<ul class="nav navbar-nav navbar-right">
+      <li><a href="<?php echo site_url('visiteur/Inscription') ?>"><span class="glyphicon glyphicon-user"></span> Inscription</a></li>
+      <li><a href="<?php echo site_url('visiteur/seConnecter') ?>"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
+    </ul>
 <?php endif; ?>
 
 
-
-<?php echo form_open('Visiteur/ResultatRechercher'); ?>
+      <?php echo form_open('Visiteur/ResultatRechercher'); ?>
       <input type="text" placeholder="Recherche.." name="recherche">
       <button type="submit">Submit</button>
-    </form>
-<?php echo form_close();?>
+      <?php echo form_close();?>
+</div>
+</nav>
+</body>
